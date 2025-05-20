479f383aad03d892a5e7e850bd5f230c561fcdc0
mahajang214 <nayanmahajan91@gmail.com>
2025-05-20 16:01:57 +0530

    transaction checked

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat");
+const hre = require("hardhat");
 const { expect } = require("chai");
 
 describe("crowdFunding", () => {
@@ -6,11 +6,12 @@ describe("crowdFunding", () => {
   let owner;
   let addr1;
   let addr2;
+  let maxDonation=hre.ethers.parseEther("100");
 
   beforeEach(async () => {
-    [owner,addr1,addr2] = await ethers.getSigners();
+    [owner,addr1,addr2] = await hre.ethers.getSigners();
 
-    const ContractFactory = await ethers.getContractFactory("crowdFunding");
+    const ContractFactory = await hre.ethers.getContractFactory("crowdFunding");
     contract = await ContractFactory.deploy();
     // await contract.deployed(); // Wait until it's mined
     await contract.waitForDeployment();
@@ -24,13 +25,47 @@ describe("crowdFunding", () => {
     expect(await contract.totalFund()).to.equal(0);
   });
 
-  // it("Donation is working.", async () => {
-  //   expect(await contract.donate(10)).to.equal(10);
-  // });
+  it("Should accept a valid donation", async function () {
+    const amount = hre.ethers.parseEther("1"); // 1 ETH
+
+    const tx = await contract.connect(addr1).donate(amount, { value: amount });
+    await tx.wait();
+
+    const donated = await contract.donations(addr1.address);
+    expect(donated).to.equal(amount);
+
+    const total = await contract.totalDonationAmount();
+    expect(total).to.equal(amount);
+  });
+
 
   // it("Withdrawl is working.",async () => {
-
-  //   expect(await contract.withdrawl(5)).to.equal(5);
+  //   const amount=hre.ethers.parseEther("5");
+  //   expect(await contract.withdrawl(amount,{value:amount})).to.equal(amount);
 
   // })
+
+  it("Withdrawl is working.", async () => {
+    const donateAmount = hre.ethers.parseEther("5");
+  
+    // addr1 donates first
+    await contract.connect(addr1).donate(donateAmount, { value: donateAmount });
+  
+    // Get initial balance of owner
+    const initialBalance = await hre.ethers.provider.getBalance(owner.address);
+  
+    // Owner withdraws funds from contract
+    const tx = await contract.connect(owner).withdrawl(donateAmount);
+    const receipt = await tx.wait();
+  
+    // Estimate gas cost
+    const gasUsed = receipt.gasUsed;
+    const gasPrice = (await hre.ethers.provider.getTransaction(tx.hash)).gasPrice;
+    const gasCost = gasUsed * gasPrice;
+  
+    const finalBalance = await hre.ethers.provider.getBalance(owner.address);
+  
+    // Expect owner's balance to increase by amount - gasCost
+    expect(finalBalance).to.equal(initialBalance.add(donateAmount).sub(gasCost));
+  });
 });
